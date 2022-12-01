083ef876ba5261cd594ba390b873b4e9055ef62e
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-01 09:23:22 +0100

    green

--- 01-calorie-coounting.spec.ts
+++ 01-calorie-coounting.spec.ts
@@ -2,22 +2,21 @@ import _ from 'lodash/fp'
 
 type accumulator = { previous: string[][], current: string[] };
 
-function mostCalories(calories: string[]): number {
-  const groupByEmptyLine = _.flow(
-    _.reduce((acc: accumulator, current: string) => {
-      if (current)
-        return {...acc, current: [...acc.current, current]};
-      return {previous: [...acc.previous, acc.current], current: []};
-    }, {previous: [], current: []}),
-    (acc: accumulator) => [...acc.previous, acc.current],
-  );
-  return _.flow(
-    groupByEmptyLine,
-    _.map(_.map(parseInt)),
-    _.map(_.sum),
-    _.sum,
-  )(calories);
-}
+const groupByEmptyLine = _.flow(
+  _.reduce((acc: accumulator, current: string) => {
+    if (current)
+      return {...acc, current: [...acc.current, current]};
+    return {previous: [...acc.previous, acc.current], current: []};
+  }, {previous: [], current: []}),
+  (acc: accumulator) => [...acc.previous, acc.current],
+);
+
+const mostCalories = _.flow(
+  groupByEmptyLine,
+  _.map(_.map(parseInt)),
+  _.map(_.sum),
+  _.sum,
+);
 
 it('no input', () => {
   expect(mostCalories([])).toEqual(0);
@@ -37,4 +36,8 @@ it('two calories', () => {
 
 it('one calorie and empty', () => {
   expect(mostCalories(['1000', ''])).toEqual(1000);
+});
+
+xit('two elves', () => {
+  expect(mostCalories(['1000', '', '2000'])).toEqual(2000);
 });
