62fcdcafa63c53bfe958d57cf4609bd74d138141
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-01 09:21:47 +0100

    green

--- 01-calorie-coounting.spec.ts
+++ 01-calorie-coounting.spec.ts
@@ -3,15 +3,16 @@ import _ from 'lodash/fp'
 type accumulator = { previous: string[][], current: string[] };
 
 function mostCalories(calories: string[]): number {
+  const groupByElves = _.flow(
+    _.reduce((acc: accumulator, current: string) => {
+      if (current)
+        return {...acc, current: [...acc.current, current]};
+      return {previous: [...acc.previous, acc.current], current: []};
+    }, {previous: [], current: []}),
+    (acc: accumulator) => [...acc.previous, acc.current],
+  );
   return _.flow(
-    _.flow(
-      _.reduce((acc: accumulator, current: string) => {
-        if (current)
-          return {...acc, current: [...acc.current, current]};
-        return {previous: [...acc.previous, acc.current], current: []};
-      }, {previous: [], current: []}),
-      (acc: accumulator) => [...acc.previous, acc.current],
-    ),
+    groupByElves,
     _.map(_.map(parseInt)),
     _.map(_.sum),
     _.sum,
