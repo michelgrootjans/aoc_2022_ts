7dfc6bf7adf4ec769715dc914e6ee5219125709f
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-01 09:44:49 +0100

    green

--- 01-calorie-coounting.spec.ts
+++ 01-calorie-coounting.spec.ts
@@ -48,6 +48,26 @@ it('two elves with multiple calories', () => {
   expect(mostCalories(['1000', '1000', '', '2000', '2000'])).toEqual(4000);
 });
 
+it('the example', () => {
+  const example = [
+    '1000',
+    '2000',
+    '3000', // 6000
+    '',
+    '4000', // 4000
+    '',
+    '5000',
+    '6000', // 11000
+    '',
+    '7000',
+    '8000',
+    '9000', // 24000
+    '',
+    '10000', // 10000
+  ];
+  expect(mostCalories(example)).toEqual(24000);
+});
+
 it('my exercise', () => {
   const input = readFileSync('./01-input.txt', 'utf-8').split(/\r?\n/);
   expect(mostCalories(input)).toEqual(70698);
