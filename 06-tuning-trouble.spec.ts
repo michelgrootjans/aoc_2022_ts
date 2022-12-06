1c41b7ccf2f4fb604c8ed5166eb7846ff3adea24
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-06 08:18:04 +0100

    06: part 2 solved

--- 06-tuning-trouble.spec.ts
+++ 06-tuning-trouble.spec.ts
@@ -13,13 +13,38 @@ function markerPosition(stream: string) {
     return -1;
 }
 
+function messagePosition(stream: string) {
+    const characters = _.split('')(stream);
+    for (let i = 0; i < characters.length - 14; i++) {
+        const mark = _.flow(
+            _.drop(i),
+            _.take(14)
+        )(characters);
+        if (mark.length === _.uniq(mark).length) return i + 14
+    }
+    return -1;
+}
+
 test.each([
     ['abcde', 4],
+    ['bvwbjplbgvbhsrlpgdmjqwftvncz', 5],
     ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7],
     ['nppdvjthqldpwncqszvftbrmjlhg', 6],
     ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10],
     ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11],
     [input, 1042],
-])("'%s' => %d", (stream, expectedMarker) => {
+])("'%s' packet starts at %d", (stream, expectedMarker) => {
     expect(markerPosition(stream)).toBe(expectedMarker);
+});
+
+test.each([
+    ['abcdefghijklmnopqrstuvwxyz', 14],
+    ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19],
+    ['bvwbjplbgvbhsrlpgdmjqwftvncz', 23],
+    ['nppdvjthqldpwncqszvftbrmjlhg', 23],
+    ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29],
+    ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26],
+    [input, 2980],
+])("'%s' message starts at %d", (stream, expectedMarker) => {
+    expect(messagePosition(stream)).toBe(expectedMarker);
 });
