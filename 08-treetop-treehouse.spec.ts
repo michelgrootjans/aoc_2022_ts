c004d286e728835c85668a13589a68bc7f48eb67
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-08 09:34:57 +0100

    green

--- 08-treetop-treehouse.spec.ts
+++ 08-treetop-treehouse.spec.ts
@@ -1,10 +1,20 @@
 import _ from 'lodash/fp'
 
-const visibleTrees = _.flow(
+function visibleTrees(trees: number[][]) {
+    return _.flow(
         _.flatten,
         _.sum,
-    )
+    )(trees)
+}
 
-it('should ', () => {
+it('1 tree', () => {
     expect(visibleTrees([[1]])).toBe(1)
+});
+
+it('square with all visible trees', () => {
+    expect(visibleTrees([
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+    ])).toBe(9)
 });
