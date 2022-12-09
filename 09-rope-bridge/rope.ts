22ab14b95e5339c085f14300c9579e94d4a0dda1
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-09 15:12:08 +0100

    green

--- 09-rope-bridge/rope.ts
+++ 09-rope-bridge/rope.ts
@@ -4,8 +4,8 @@ import {Knot} from "./knot";
 export class Rope {
     private knots: Knot[];
 
-    static build(numberOfKnots = 2) {
-        return new Rope(_.range(numberOfKnots).map(_ => new Knot(0,0)))
+    static build() {
+        return new Rope(_.range(2).map(_ => new Knot(0,0)))
     }
 
     private constructor(knots: Knot[]) {
@@ -41,6 +41,12 @@ export class Rope {
     }
 
     private move(head: Knot): Rope {
+        function abc([head, next, ...rest]: Knot[]): Knot[] {
+            if(!next) return [];
+            return [head, ...abc([next.follow(head), ...rest])];
+        }
+
+        const knots: Knot[] = abc([head, ..._.tail(this.knots)])
         return new Rope([head, this.tail().follow(head)]);
     }
 }
