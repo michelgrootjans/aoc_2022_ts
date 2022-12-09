4550c8b6ebd71b5ded1d1f73c63660f0a80b2d06
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-09 08:19:57 +0100

    green

--- 09-rope-bridge.spec.ts
+++ 09-rope-bridge.spec.ts
@@ -1,5 +1,3 @@
-import _ from 'lodash/fp'
-
 class Command {
     public readonly direction: string;
     public readonly steps: number;
@@ -12,11 +10,11 @@ class Command {
 }
 
 function positionsOfTail(commands: Command[]) {
-    return _.flow(
-        _.map('steps'),
-        _.sum
-    )(commands) || 1;
+    return commands.reduce((sum, command) => sum + command.steps, 0) || 1;
 }
+
+const right = (steps: number) => new Command('R', steps);
+const left = (steps: number) => new Command('D', steps);
 
 test('no moves', function () {
     expect(positionsOfTail([])).toBe(1);
@@ -41,6 +39,9 @@ test('R 1, R1', function () {
     ])).toBe(2);
 });
 
-function right(steps: number) {
-    return new Command('R', steps);
-}
+xtest('R 1, L1', function () {
+    expect(positionsOfTail([
+        right(1),
+        left(1),
+    ])).toBe(1);
+});
