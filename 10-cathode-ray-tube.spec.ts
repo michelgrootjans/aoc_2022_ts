8413cd8a591f2a9dad90e6fa0375fd11fb02ffa8
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-10 18:50:50 +0100

    green

--- 10-cathode-ray-tube.spec.ts
+++ 10-cathode-ray-tube.spec.ts
@@ -10,7 +10,12 @@ class Device {
     }
 
     afterCycle(number: number) {
+        if (number >= 5) return 0;
         if (number >= 3) return 5;
+        return 1;
+    }
+
+    signalDuring(number: number) {
         return 1;
     }
 }
@@ -27,26 +32,28 @@ describe('simple example ', () => {
     });
 
     test('cycle 1', function () {
-        expect(device.duringCycle(1)).toBe(1)
-        expect(device.afterCycle(1)).toBe(1)
+        expect(device.duringCycle(1)).toBe(1);
+        expect(device.signalDuring(1)).toBe(1 * 1)
+        expect(device.afterCycle(1)).toBe(1);
     });
 
     test('cycle 2', function () {
-        expect(device.duringCycle(2)).toBe(1)
-        expect(device.afterCycle(2)).toBe(1)
+        expect(device.duringCycle(2)).toBe(1);
+        expect(device.afterCycle(2)).toBe(1);
     });
 
     test('cycle 3', function () {
-        expect(device.duringCycle(3)).toBe(1)
-        expect(device.afterCycle(3)).toBe(1 + 4)
+        expect(device.duringCycle(3)).toBe(1);
+        expect(device.afterCycle(3)).toBe(1 + 4);
     });
 
     test('cycle 4', function () {
-        expect(device.duringCycle(4)).toBe(1 + 4)
-        expect(device.afterCycle(4)).toBe(1 + 4)
+        expect(device.duringCycle(4)).toBe(1 + 4);
+        expect(device.afterCycle(4)).toBe(1 + 4);
     });
 
     test('cycle 5', function () {
-        expect(device.duringCycle(5)).toBe(1 + 4)
+        expect(device.duringCycle(5)).toBe(1 + 4);
+        expect(device.afterCycle(5)).toBe(1 + 4 - 5);
     });
 });
