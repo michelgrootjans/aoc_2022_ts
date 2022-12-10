26243da40fba229df82d8535df0f3b12b5c80d9e
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-10 18:09:51 +0100

    green

--- 10-cathode-ray-tube.spec.ts
+++ 10-cathode-ray-tube.spec.ts
@@ -1,3 +1,19 @@
-it('should work', function () {
-    
+class Device {
+    constructor(input: string[]) {
+
+    }
+
+
+    duringCycle(number: number): number {
+        return 1;
+    }
+}
+
+it('simple example', function () {
+    const input = [
+        'noop',
+        'addx 3',
+        'addx -5',
+    ]
+    expect(new Device(input).duringCycle(1)).toBe(1)
 });
