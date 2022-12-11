ce93d15abb192d4591b968258778e280aa34415a
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-11 14:53:01 +0100

    refactor

--- 11-monkey-in-the-middle.spec.ts
+++ 11-monkey-in-the-middle.spec.ts
@@ -222,7 +222,7 @@ describe('turn (all inspections of a single monkey)', () => {
         })
     });
     test('three turns', function () {
-        expect(exampleMonkeys.turn().turn().turn()).toMatchObject({
+        expect(exampleMonkeys.turn(3)).toMatchObject({
             monkeys: [
                 {items: [20, 23, 27, 26], inspections: 2},
                 {items: [2080], inspections: 4},
