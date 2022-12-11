500e9b337d25439f6a7836987d9a77848804fcb6
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-11 10:32:31 +0100

    refactor

--- 11-monkey-in-the-middle.spec.ts
+++ 11-monkey-in-the-middle.spec.ts
@@ -1,3 +1,5 @@
+import {exampleDescription} from "./11-example";
+
 class Monkey {
     public readonly items: number[];
 
@@ -41,35 +43,6 @@ function parseMonkey(monkeyDescription: string): Monkey {
 const splitMonkeys = (monkeyDescriptions: string) => monkeyDescriptions.split('\n\n');
 
 const parseMonkeys = (monkeyDescriptions: string): Monkeys => new Monkeys(splitMonkeys(monkeyDescriptions).map(parseMonkey));
-
-const exampleDescription = '' +
-    "Monkey 0:\n" +
-    "  Starting items: 79, 98\n" +
-    "  Operation: new = old * 19\n" +
-    "  Test: divisible by 23\n" +
-    "    If true: throw to monkey 2\n" +
-    "    If false: throw to monkey 3\n" +
-    "\n" +
-    "Monkey 1:\n" +
-    "  Starting items: 54, 65, 75, 74\n" +
-    "  Operation: new = old + 6\n" +
-    "  Test: divisible by 19\n" +
-    "    If true: throw to monkey 2\n" +
-    "    If false: throw to monkey 0\n" +
-    "\n" +
-    "Monkey 2:\n" +
-    "  Starting items: 79, 60, 97\n" +
-    "  Operation: new = old * old\n" +
-    "  Test: divisible by 13\n" +
-    "    If true: throw to monkey 1\n" +
-    "    If false: throw to monkey 3\n" +
-    "\n" +
-    "Monkey 3:\n" +
-    "  Starting items: 74\n" +
-    "  Operation: new = old + 3\n" +
-    "  Test: divisible by 17\n" +
-    "    If true: throw to monkey 0\n" +
-    "    If false: throw to monkey 1";
 
 const exampleMonkeys = parseMonkeys(exampleDescription);
 
