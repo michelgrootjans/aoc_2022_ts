a033cfb6436768101eb5891e25d19e65ada32420
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-11 15:14:10 +0100

    green

--- 11-monkey-in-the-middle.spec.ts
+++ 11-monkey-in-the-middle.spec.ts
@@ -269,10 +269,10 @@ describe('round (all turns of all monkeys)', () => {
         })
     });
     test('three rounds', () => {
-        expect(exampleMonkeys.round(2).turn().inspect(1)).toMatchObject({
+        expect(exampleMonkeys.round(2).turn().inspect(2)).toMatchObject({
             monkeys: [
-                {items: [16], inspections: 11},
-                {items: [49, 58, 55, 362], inspections: 11},
+                {items: [16, 18], inspections: 11},
+                {items: [58, 55, 362], inspections: 12},
                 {items: [], inspections: 4},
                 {items: [4401, 63, 449, 855, 2216], inspections: 10},
             ]
