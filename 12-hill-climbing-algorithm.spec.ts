d6dec8157837998d60ebaccfc217ca9a298f3e8d
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-12 16:49:03 +0100

    red

--- 12-hill-climbing-algorithm.spec.ts
+++ 12-hill-climbing-algorithm.spec.ts
@@ -5,3 +5,13 @@ function climb(map: string) {
 it('a straight line', () => {
     expect(climb('SbcdefghijklmnopqrstuvwxyE')).toBe(26)
 });
+
+it('example - part 1', () => {
+    const input = '' +
+        'Sabqponm\n' +
+        'abcryxxl\n' +
+        'accszExk\n' +
+        'acctuvwj\n' +
+        'abdefghi'
+    expect(climb(input)).toBe(31)
+});
