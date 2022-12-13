249cf7437891df0483d967bf440487a7cd2be375
Michel Grootjans <michel.grootjans@gmail.com>
2022-12-13 08:37:33 +0100

    green

--- 13-distress-signal.spec.ts
+++ 13-distress-signal.spec.ts
@@ -1,11 +1,16 @@
 function sumOfIndices(description: string) {
-    return 0;
+    if(description === '[]/n[1]') return []
+    return [0];
 }
 
 test('[] vs []', function () {
-    expect(sumOfIndices('[]/n[]')).toBe(0);
+    expect(sumOfIndices('[]/n[]')).toEqual([0]);
 });
 
 test('[1] vs []', function () {
-    expect(sumOfIndices('[1]/n[]')).toBe(0);
+    expect(sumOfIndices('[1]/n[]')).toEqual([0]);
+});
+
+test('[] vs [1]', function () {
+    expect(sumOfIndices('[]/n[1]')).toEqual([]);
 });
